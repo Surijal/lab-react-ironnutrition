084e0d26569dd3e46a5f61453f2075d5821130ab
Boris Vogt <boris.vogt@posteo.de>
2019-12-05 10:43:05 +0100

    search solve after review

--- starter-code/src/componets/Foods.js
+++ starter-code/src/componets/Foods.js
@@ -1,67 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import FoodBox from './FoodBox';
 import AddNewFood from './AddNewFood';
 import Search from './Search';
 import foods from '../foods.json';
 import shortid from 'shortid';
 
-foods.forEach((food)=> {food.id = shortid.generate()});
-
-
+foods.forEach(food => {
+food.id = shortid.generate();
+});
 
 class Foods extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            foods: foods,
-            filter: foods,
-            form: false
-        }
-    } 
-    
-
-    addFood = (foodObj) => {
-        const updatedFood =[foodObj, ...this.state.foods];
-
-        this.setState({foods: updatedFood});
-    }
-
-
-    searchOneFood= () => {
-
-        const filterfood = (search) => {this.state.foods.filter( el => el.name.toLowerCase().includes(search.toLowerCase()) > - 1)} 
-
-        console.log('HELLO FILTERFOOD', {filterfood});
-        
-
-        this.setState({filter: filterfood})
-    }
-
-    
-
-
-    render() {
-        return (
-            <div>
-                <Search searchOneFood={this.searchOneFood}/>
-
-            
-
-                <AddNewFood addOneFood={this.addFood}/>
-            
-                {
-                this.state.foods.map((foodObj, index) =>{
-                    return(
-                    <FoodBox key={index} {...foodObj}/>  
-                    );
-                })  
-                };
-
-            </div>
-        )
-    }
+constructor(props) {
+    super(props);
+    this.state = {
+    foods: foods,
+    filter: foods,
+    form: false,
+    };
 }
 
+addFood = foodObj => {
+    const updatedFood = [foodObj, ...this.state.foods];
+
+    this.setState({ foods: updatedFood });
+};
+
+searchOneFood = query => {
+    const ifMatch = (query, foodName) => {
+    return foodName.toLowerCase().includes(query.toLowerCase());
+    };
+
+    const filter = this.state.foods.filter(food => {
+    return ifMatch(query, food.name);
+    //   return foodName.toLowerCase().includes(query.toLowerCase());
+    });
+
+    this.setState({ filter });
+};
+
+render() {
+    return (
+    <div>
+        <Search searchOneFood={this.searchOneFood} />
+        <AddNewFood addOneFood={this.addFood} />
+        {this.state.filter.map((foodObj, index) => {
+        return <FoodBox key={index} {...foodObj} />;
+        })}
+        ;
+    </div>
+    );
+}
+}
 
 export default Foods;
